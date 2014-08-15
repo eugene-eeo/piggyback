8959f84c44b5db3c353cda4b3bd386f20dff4e82
Eugene Eeo <xe0jun@hotmail.com>
2014-08-15 15:34:02 +0800

    license and more docs

--- piggyback/__init__.py
+++ piggyback/__init__.py
@@ -1,1 +1,12 @@
+"""
+    piggyback
+    ~~~~~~~~~
+
+    Easily import modules, and stop it with the ugly
+    ``execfiles`` and manual ``sys.path`` management.
+
+    :copyright: (c) 2014 Eugene Eeo
+    :license: MIT, see LICENSE for details.
+"""
+
 __version__ = '0.1'
