6cb5a65411be7fe5d1411c1b7746a5dd37cd5fd0
Eugene Eeo <xe0jun@hotmail.com>
2014-08-16 11:22:07 +0800

    regression tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,2 @@
-from tests.test_loader import LoaderTest
+from tests.test_loader import FileLoaderTest, DirectoryLoaderTest
 from tests.test_finder import FinderTest
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -2,17 +2,17 @@ import unittest
 from piggyback.finder import Finder
 from piggyback.loader import Loader
 
+dirloader = Loader(Finder('tests/examples'))
+fileloader = Loader(Finder('tests/example.py'))
 
-class LoaderTest(unittest.TestCase):
-    def setUp(self):
-        self.loader = Loader(Finder('tests/examples'))
 
+class DirectoryLoaderTest(unittest.TestCase):
     def test_look(self):
-        found = set(self.loader.search())
+        found = set(dirloader.search())
         assert found == set(('examples.module', 'examples.nested.module'))
 
     def test_import_all(self):
-        cache = self.loader.import_all()
+        cache = dirloader.import_all()
         for item in ('examples.module', 'examples.nested.module'):
             assert item in cache
 
@@ -20,5 +20,16 @@ class LoaderTest(unittest.TestCase):
         assert cache['examples.module'].__name__ == 'examples.module'
 
     def test_load(self):
-        assert self.loader.load('examples.module').this
-        assert self.loader.load('examples.nested.module')
+        assert dirloader.load('examples.module').this
+        assert dirloader.load('examples.nested.module')
+
+
+class FileLoaderTest(unittest.TestCase):
+    def test_look(self):
+        found = set(fileloader.search())
+        assert found == set(('example',))
+
+    def test_import_all(self):
+        cache = fileloader.import_all()
+        assert len(cache) == 1
+        assert cache['example'].__name__ == 'example'
