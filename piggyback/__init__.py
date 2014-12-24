100f898b7f24d528b23cc51607c76e0442aa77b6
Eugene Eeo <xe0jun@hotmail.com>
2014-12-24 22:02:03 +0800

    the code is so meta

--- piggyback/__init__.py
+++ piggyback/__init__.py
@@ -1,8 +1,28 @@
+"""
+    piggyback
+    ~~~~~~~~~
+
+    A sane, robust, object oriented API to find and
+    import modules recursively.
+
+    :license: MIT, see LICENSE for details
+    :copyright: 2014 Eugene Eeo
+"""
+
+
 from os.path import isdir as _isdir
 from piggyback.finder import FileFinder, ModuleFinder
 from piggyback.loader import Loader
 
 
 def loader(path):
+    """
+    Returns a loader object for a given path, with
+    the correct finder class (``ModuleFinder`` for
+    directories and ``FileFinder`` for single file
+    modules).
+
+    :param path: The path to the module(s).
+    """
     finder = ModuleFinder if _isdir(path) else FileFinder
     return Loader(finder(path))
