dc1c392fbd148840098c4646e896a3860619d989
Eugene Eeo <xe0jun@hotmail.com>
2014-12-25 13:01:07 +0800

    modify loader

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -8,17 +8,17 @@ def test_search(loader):
 
 def test_import_all(loader):
     prev = loader.import_all()['tests.example']
+    curr = loader.import_all()
 
-    cache = loader.import_all()
+    assert 'tests.conftest' in curr
+    assert 'tests.example' in curr
 
-    assert 'tests.conftest' in cache
-    assert 'tests.example' in cache
+    example = curr['tests.example']
 
-    assert cache['tests.example'].const == 5
-    assert cache['tests.example'].delta is prev.delta
+    assert example.const == 5
+    assert example.delta is prev.delta
 
 
 def test_import_module(loader):
     example = loader.import_module('tests.example')
-
     assert example.const == 5
